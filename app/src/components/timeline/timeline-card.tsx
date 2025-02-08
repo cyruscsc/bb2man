e0dac1f5c665347513f4b99ea4d82e445327491a
Cyrus Chan <cyruschanca@gmail.com>
2025-02-07 19:59:13 -0500

    improve timeline's responsiveness

--- app/src/components/timeline/timeline-card.tsx
+++ app/src/components/timeline/timeline-card.tsx
@@ -7,13 +7,15 @@ interface CardProps {
 
 const TimelineCard = ({ date, title, description, position }: CardProps) => {
   return (
-    <li className={`timeline-card ${position}`}>
+    <li className='timeline-card-outer'>
       <div className='timeline-pointer'></div>
-      <div className='timeline-card-header'>
-        <h2 className='timeline-card-date'>{date}</h2>
-        <h2 className='timeline-card-title'>{title}</h2>
+      <div className={`timeline-card ${position}`}>
+        <div className='timeline-card-header'>
+          <h2 className='timeline-card-date'>{date}</h2>
+          <h2 className='timeline-card-title'>{title}</h2>
+        </div>
+        <p className='timeline-card-description'>{description}</p>
       </div>
-      <p className='timeline-card-description'>{description}</p>
     </li>
   )
 }
