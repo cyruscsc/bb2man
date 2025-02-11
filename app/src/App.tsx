a2f64d304c3c3c445f18b70bf9474862773327ac
Cyrus Chan <cyruschanca@gmail.com>
2025-02-10 19:54:39 -0500

    add html and pwa configs

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,6 @@
 import { Carousel } from './components/carousel'
 import { AppWrapper, Footer, Header } from './components/core'
 import { Timeline } from './components/timeline'
-import { events } from './data'
 
 const App = () => {
   return (
